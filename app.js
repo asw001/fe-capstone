d32374ba6fa8513a0a70be68f79f260fe024794d
asw001 <variableapple@gmail.com>
2017-05-28 20:59:26 -0500

    basic sign-in functionality

---
+++ app.js
@@ -0,0 +1,25 @@
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());   // This is null if the 'email' scope is not present.
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+};
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+  console.log('User signed out.');
+   });
+};
+
+$(function() {
+
+$('div.nav-bar').on('click', 'button#sign-out', ( function(event) {
+  event.preventDefault();
+  signOut();
+  }));
+});
