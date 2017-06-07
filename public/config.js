105451febd2ffc6bbcc78c364b9def4c90c11120
asw001 <variableapple@gmail.com>
2017-06-06 23:02:22 -0500

    clean up

--- public/config.js
+++
@@ -1,5 +0,0 @@
-taAppConfig = {
-  apiKey: "AIzaSyDiEhP9ppjeKFrWYj1PcsJhlLmO_Nq9Huk",
-  authDomain: "thinkful-fe-cs-asw.firebaseapp.com",
-  databaseURL: "https://thinkful-fe-cs-asw.firebaseio.com",
-};
