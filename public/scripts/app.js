c4913a04449f88721941bd0f69aa790b0f2d5fc9
asw001 <variableapple@gmail.com>
2017-06-12 19:23:19 -0500

    melded config.js with app and index

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -39,6 +39,19 @@ function handleFormReveal() {
   });
 };
 
+taAppConfig = {
+  apiKey: "AIzaSyDiEhP9ppjeKFrWYj1PcsJhlLmO_Nq9Huk",
+  authDomain: "thinkful-fe-cs-asw.firebaseapp.com",
+  databaseURL: "https://thinkful-fe-cs-asw.firebaseio.com",
+};
+
+renderConfig = {
+  quoteDivTemplate: '<div class="quote-container"></div>',
+  authorTemplate: '<div class="signature"></div>',
+  slideDiv: 'div#slideshow',
+  slideChildDiv: '<div style="display: none"></div>',
+}
+
 //instantiates the database object
 function initDB() {
     var database = firebase.database();
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,18 @@
 var authElem = '#firebaseui-auth-container';
 
+taAppConfig = {
+  apiKey: "AIzaSyDiEhP9ppjeKFrWYj1PcsJhlLmO_Nq9Huk",
+  authDomain: "thinkful-fe-cs-asw.firebaseapp.com",
+  databaseURL: "https://thinkful-fe-cs-asw.firebaseio.com",
+};
+
+renderConfig = {
+  quoteDivTemplate: '<div class="quote-container"></div>',
+  authorTemplate: '<div class="signature"></div>',
+  slideDiv: 'div#slideshow',
+  slideChildDiv: '<div style="display: none"></div>',
+}
+
 function doFireBaseInit() {
     firebase.initializeApp(taAppConfig);
     try {
